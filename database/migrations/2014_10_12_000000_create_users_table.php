bcca45502d8d93fbf4334b2ccb4eb510f82ff1f8
Russel Claveria <rmclaveria@iskolarngbayan.pup.edu.ph>
2021-06-02 13:13:38 +0800

    intro commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedTinyInt('role');
+            $table->string('email')->unique();
+            $table->string('password');
+
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->date('birth_date');
+            $table->text('address');
+            $table->string('contact_number')->unique();
+            
+            $table->timestamp('email_verified_at')->nullable();
+            
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
