d70f8db3b78bdfa93c41b29a908439fac7803d8b
harajuku99 <52187209+harajuku99@users.noreply.github.com>
2021-07-02 01:34:53 +0800

    shop filter complete

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -9,6 +9,8 @@ use \App\Models\Brand;
 use \App\Models\Shoe;
 use \App\Models\Stock;
 use \App\Models\Size;
+use \App\Models\Category;
+use \App\Models\Type;
 
 class ShopController extends Controller
 {
@@ -20,28 +22,134 @@ class ShopController extends Controller
     {
         //$this->middleware('auth');
         $brand = Brand::all();
+        $type = Type::all();
+        $category = Category::all();
 
-        $brands = Brand::with(['shoes' => function ($query) {
 
-            $query->orderBy('created_at', 'DESC');}, 
-            'shoes.shoeImages' => function ($query) {
-            $query->where('image_angle_id', '3')->pluck('image');},])
-        ->orderBy('name', 'ASC')
-        ->get();
+        $shoe =  DB::table('shoes')
+            ->join('brands', 'brands.brand_id', '=', 'shoes.brand_id')
+            ->join('shoe_images', 'shoe_images.shoe_id', '=', 'shoes.shoe_id')
+            ->select('shoes.name as shoes', 'shoes.shoe_id as shoe_id', 'brands.name as brand',
+            'brands.brand_id as brand_id','shoes.description as description', 
+            'shoe_images.image_angle_id as image_angle_id', 'shoes.price as price', 'shoe_images.image as image')
+            ->where('image_angle_id', '3')
+            ->orderBy('shoes.name', 'ASC')
+            ->get();
         
-        if(Request::get('filterbrand')){
-
-            $checked = $_GET['filterbrand'];
-            $brands = Brand::with(['shoes' => function ($query) {
-                $query->orderBy('created_at', 'DESC');}, 
-                'shoes.shoeImages' => function ($query) {
-                $query->where('image_angle_id', '3')->pluck('image');},])
-            ->whereIn('brand_id', $checked)
-            ->orderBy('name', 'ASC')
+        if(Request::get('filterbrand')&&Request::get('filtercategory')&&Request::get('filtertype')){
+            $filbrand = $_GET['filterbrand'];
+            $filcategory = $_GET['filtercategory'];
+            $filtertype = $_GET['filtertype'];
+            $shoe =  DB::table('shoes')
+            ->join('brands', 'brands.brand_id', '=', 'shoes.brand_id')
+            ->join('shoe_images', 'shoe_images.shoe_id', '=', 'shoes.shoe_id')
+            ->select('shoes.name as shoes', 'shoes.shoe_id as shoe_id', 'brands.name as brand',
+            'brands.brand_id as brand_id','shoes.description as description', 
+            'shoe_images.image_angle_id as image_angle_id', 'shoes.price as price', 
+            'shoe_images.image as image')
+            ->where('image_angle_id', '3')
+            ->whereIn('shoes.category_id', $filcategory)
+            ->whereIn('shoes.brand_id', $filbrand)
+            ->whereIn('shoes.type_id', $filtertype)
+            ->orderBy('shoes.name', 'ASC')
+            ->get();
+        }
+        elseif(Request::get('filterbrand')&&Request::get('filtercategory')){
+            $filbrand = $_GET['filterbrand'];
+            $filcategory = $_GET['filtercategory'];
+    
+            $shoe =  DB::table('shoes')
+            ->join('brands', 'brands.brand_id', '=', 'shoes.brand_id')
+            ->join('shoe_images', 'shoe_images.shoe_id', '=', 'shoes.shoe_id')
+            ->select('shoes.name as shoes', 'shoes.shoe_id as shoe_id', 'brands.name as brand',
+            'brands.brand_id as brand_id','shoes.description as description', 
+            'shoe_images.image_angle_id as image_angle_id', 'shoes.price as price', 
+            'shoe_images.image as image')
+            ->where('image_angle_id', '3')
+            ->whereIn('shoes.category_id', $filcategory)
+            ->whereIn('shoes.brand_id', $filbrand)
+            ->orderBy('shoes.name', 'ASC')
+            ->get();
+        }
+        elseif(Request::get('filterbrand')&&Request::get('filtertype')){
+            $filbrand = $_GET['filterbrand'];
+            $filtertype = $_GET['filtertype'];
+    
+            $shoe =  DB::table('shoes')
+            ->join('brands', 'brands.brand_id', '=', 'shoes.brand_id')
+            ->join('shoe_images', 'shoe_images.shoe_id', '=', 'shoes.shoe_id')
+            ->select('shoes.name as shoes', 'shoes.shoe_id as shoe_id', 'brands.name as brand',
+            'brands.brand_id as brand_id','shoes.description as description', 
+            'shoe_images.image_angle_id as image_angle_id', 'shoes.price as price', 
+            'shoe_images.image as image')
+            ->where('image_angle_id', '3')
+            ->whereIn('shoes.type_id', $filtertype)
+            ->whereIn('shoes.brand_id', $filbrand)
+            ->orderBy('shoes.name', 'ASC')
+            ->get();
+        }
+        elseif(Request::get('filtercategory')&&Request::get('filtertype')){
+            $filcategory = $_GET['filtercategory'];
+            $filtertype = $_GET['filtertype'];
+    
+            $shoe =  DB::table('shoes')
+            ->join('brands', 'brands.brand_id', '=', 'shoes.brand_id')
+            ->join('shoe_images', 'shoe_images.shoe_id', '=', 'shoes.shoe_id')
+            ->select('shoes.name as shoes', 'shoes.shoe_id as shoe_id', 'brands.name as brand',
+            'brands.brand_id as brand_id','shoes.description as description', 
+            'shoe_images.image_angle_id as image_angle_id', 'shoes.price as price', 
+            'shoe_images.image as image')
+            ->where('image_angle_id', '3')
+            ->whereIn('shoes.type_id', $filtertype)
+            ->whereIn('shoes.category_id', $filcategory)
+            ->orderBy('shoes.name', 'ASC')
+            ->get();
+        }
+        elseif(Request::get('filtercategory')){
+            $filcategory = $_GET['filtercategory'];
+            
+            $shoe =  DB::table('shoes')
+            ->join('brands', 'brands.brand_id', '=', 'shoes.brand_id')
+            ->join('shoe_images', 'shoe_images.shoe_id', '=', 'shoes.shoe_id')
+            ->select('shoes.name as shoes', 'shoes.shoe_id as shoe_id', 'brands.name as brand',
+            'brands.brand_id as brand_id','shoes.description as description', 
+            'shoe_images.image_angle_id as image_angle_id', 'shoes.price as price', 
+            'shoe_images.image as image')
+            ->whereIn('shoes.category_id', $filcategory)
+            ->where('image_angle_id', '3')
+            ->orderBy('shoes.name', 'ASC')
+            ->get();
+        }
+        elseif(Request::get('filterbrand')){
+            $filbrand = $_GET['filterbrand'];
+            $shoe =  DB::table('shoes')
+            ->join('brands', 'brands.brand_id', '=', 'shoes.brand_id')
+            ->join('shoe_images', 'shoe_images.shoe_id', '=', 'shoes.shoe_id')
+            ->select('shoes.name as shoes', 'shoes.shoe_id as shoe_id', 'brands.name as brand',
+            'brands.brand_id as brand_id','shoes.description as description', 
+            'shoe_images.image_angle_id as image_angle_id', 'shoes.price as price', 
+            'shoe_images.image as image')
+            ->where('image_angle_id', '3')
+            ->whereIn('shoes.brand_id', $filbrand)
+            ->orderBy('shoes.name', 'ASC')
+            ->get();
+        }
+        elseif(Request::get('filtertype')){
+            $filtertype = $_GET['filtertype'];
+            $shoe =  DB::table('shoes')
+            ->join('brands', 'brands.brand_id', '=', 'shoes.brand_id')
+            ->join('shoe_images', 'shoe_images.shoe_id', '=', 'shoes.shoe_id')
+            ->select('shoes.name as shoes', 'shoes.shoe_id as shoe_id', 'brands.name as brand',
+            'brands.brand_id as brand_id','shoes.description as description', 
+            'shoe_images.image_angle_id as image_angle_id', 'shoes.price as price', 
+            'shoe_images.image as image')
+            ->where('image_angle_id', '3')
+            ->whereIn('shoes.type_id', $filtertype)
+            ->orderBy('shoes.name', 'ASC')
             ->get();
         }
     
-        return view('shop.index', compact('brand', 'brands'));
+        return view('shop.index', compact('brand', 'shoe', 'category', 'type'));
     }
 
 }
