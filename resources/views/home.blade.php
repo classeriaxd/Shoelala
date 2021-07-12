92028175853a5d51ba8da058572468f2207fcc3d
Russel Claveria <rmclaveria@iskolarngbayan.pup.edu.ph>
2021-07-12 13:21:36 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,26 +7,24 @@
             <div class="col-md-12">
         @role('Super Admin')
                 <h3 class="display-4 text-center">Currently logged as {{Auth::user()->first_name}}</h3>
-                <small>Super Admin</small>
+                <h6 class="text-muted text-center">Super Admin</h6>
                 <hr>
 
                 <h6 class="display-6 text-center">What do you want to do for today?</h6>
-                <div class="row justify-content-center"> {{-- Collapse Buttons --}}
-                    <a class="btn btn-outline-link mr-5" data-toggle="collapse" href="#collapseOrders" role="button" aria-expanded="false" aria-controls="collapseOrders">
+                <div class="row justify-content-center mb-1"> {{-- Collapse Buttons --}}
+                    <a class="btn btn-outline-dark mr-5" data-toggle="collapse" href="#collapseOrders" role="button" aria-expanded="false" aria-controls="collapseOrders">
                     Orders
                     </a>
-                    <a class="btn btn-outline-link mr-5" data-toggle="collapse" href="#collapseStocks" role="button" aria-expanded="false" aria-controls="collapseStocks">
+                    <a class="btn btn-outline-dark mr-5" data-toggle="collapse" href="#collapseStocks" role="button" aria-expanded="false" aria-controls="collapseStocks">
                     Stocks
                     </a>
-                    <a class="btn btn-outline-link mr-5" data-toggle="collapse" href="#collapseReports" role="button" aria-expanded="false" aria-controls="collapseReports">
+                    <a class="btn btn-outline-dark mr-5" data-toggle="collapse" href="#collapseReports" role="button" aria-expanded="false" aria-controls="collapseReports">
                     Reports
                     </a>
-                    <a class="btn btn-outline-link" data-toggle="collapse" href="#collapseMaintenance" role="button" aria-expanded="false" aria-controls="collapseMaintenance">
+                    <a class="btn btn-outline-dark" data-toggle="collapse" href="#collapseMaintenance" role="button" aria-expanded="false" aria-controls="collapseMaintenance">
                     Maintenance
                     </a>
                 </div>
-
-                <br>
 
                 <div class="row justify-content-center mb-2"> {{-- Orders --}}
                     <div class="collapse" id="collapseOrders">
@@ -77,32 +75,32 @@
 
         @elserole('Admin')
                     <h3 class="display-4 text-center">Currently logged as {{Auth::user()->first_name}}</h3>
-                    <small>Cashier</small>
+                    <h6 class="text-muted text-center">Cashier</h6>
                     <hr>
                     <h6 class="display-6 text-center">What do you want to do for today?</h6>
                     <div class="row justify-content-center"> {{-- Collapse Buttons --}}
-                        <a class="btn btn-outline-link mr-5" data-toggle="collapse" href="#collapseOrders-cashier" role="button" aria-expanded="false" aria-controls="collapseOrders-cashier">
+                        <a class="btn btn-outline-dark mr-5" data-toggle="collapse" href="#collapseOrders-cashier" role="button" aria-expanded="false" aria-controls="collapseOrders-cashier">
                         Orders
                         </a>
-                        <a class="btn btn-outline-link mr-5" data-toggle="collapse" href="#collapseStocks-cashier" role="button" aria-expanded="false" aria-controls="collapseStocks-cashier">
+                        <a class="btn btn-outline-dark" data-toggle="collapse" href="#collapseStocks-cashier" role="button" aria-expanded="false" aria-controls="collapseStocks-cashier">
                         Stocks
                         </a>
                     </div>    
                     <div class="row justify-content-center mb-2"> {{-- Orders --}}
                         <div class="collapse" id="collapseOrders-cashier">
                             <a class="btn btn-link" href="#">Pending Orders of this week</a>
-                            <a class="btn btn-link" href="#">Scan QR</a>
+                            <a class="btn btn-link" href="/orders/scan">Scan QR</a>
                         </div>
                     </div>
                     <div class="row justify-content-center mb-2"> {{-- Stocks --}}
-                        <div class="collapse" id="collapseOrders-cashier">
-                            <a class="btn btn-link" href="#">View Stocks</a>
+                        <div class="collapse" id="collapseStocks-cashier">
+                            <a class="btn btn-link" href="/stocks">View Stocks</a>
                         </div>
                     </div>
 
         @elserole('User')
                     <h3 class="display-4 text-center">Currently logged as {{Auth::user()->first_name}}</h3>
-                    <small>User</small>
+                    <h6 class="text-muted text-center">User</h6>
                         <div class="card">
                             <div class="card-header">Choices</div>
                             <div class="card-body text-center">
