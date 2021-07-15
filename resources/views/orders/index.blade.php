9782238a7432e78045c951f689ef40e85f09458d
Russel Claveria <rmclaveria@iskolarngbayan.pup.edu.ph>
2021-07-15 18:59:06 +0800

    superadmin-ordersview

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -3,31 +3,144 @@
 @section('content')
 <div class="container" style="margin-top: 100px;" id="main-container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <h2 class="display-2 text-center">Order Index</h2>
             <a href="/home">
                 <button class="btn btn-secondary col-md-12 btn-lg mb-2">Go back</button>
             </a>
-                <div class="card">
-                    <div class="card-header text-center">{{ __('Order Dashboard') }}</div>
-                    <div class="card-body">
-                        @role('Super Admin')
-                            <a href="/orders/scan" class="mb-2">
-                                <button class="btn btn-primary col-md-12 btn-lg mb-2">Scan QR</button>
-                            </a>
-                            <a href="/orders/e">
-                                <button class="btn btn-primary col-md-12 btn-lg">Tag Expired Orders</button>
-                            </a>
-                        @elserole('Admin')
-                            <a href="/orders/scan">
-                                <button class="btn btn-primary col-md-12 btn-lg  mb-2">Scan QR</button>
-                            </a>
-                            <a href="/orders/e">
-                                <button class="btn btn-primary col-md-12 btn-lg">Tag Expired Orders</button>
-                            </a>
-                        @endrole
-                    </div>
+@role('Super Admin')            
+            <div class="card">
+                <div class="card-header text-center">{{ __('Order Dashboard') }}</div>
+                <div class="card-body">
+                    <a href="/orders/scan" class="mb-2">
+                        <button class="btn btn-primary col-md-12 btn-lg mb-2">Scan QR</button>
+                    </a>
+                    <a href="/orders/e">
+                        <button class="btn btn-primary col-md-12 btn-lg">Tag Expired Orders</button>
+                    </a>
                 </div>
+            </div>
+            @php $i= 1; @endphp
+            <hr>
+            <div id="pendingOrdersTable" class="mt-4">
+                <h4 class="text-center mt-2">Pending Orders</h4>
+                <table class="table-hover table-bordered w-100 text-center">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Buyer</th>
+                            <th scope="col">Order Date</th>
+                            <th scope="col">Pickup Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                @foreach($pendingOrders as $pendingOrder)
+                    <tr>
+                        <td scope="row">{{$i}}</td>
+                        <td scope="row">{{$pendingOrder->buyer_fullName}}</td>
+                        <td scope="row">{{$pendingOrder->order_date}}</td>
+                        <td scope="row">{{$pendingOrder->pickup_date}}</td>
+                    </tr>
+                @php $i += 1; @endphp
+                @endforeach
+                    </tbody>
+                </table>
+            </div>
+            @php $i= 1; @endphp
+            <hr>
+            <div id="completedOrdersTable" class="mt-3">
+                <h4 class="text-center mt-2">Completed Orders</h4>
+               <table class="table-hover table-bordered w-100 text-center">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Buyer</th>
+                            <th scope="col">Order Date</th>
+                            <th scope="col">Completed Date</th>
+                            <th scope="col">Completed By</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($completedOrders as $completedOrder)
+                        <tr>
+                            <td scope="row">{{$i}}</td>
+                            <td>{{$completedOrder->buyer_fullName}}</td>
+                            <td>{{$completedOrder->order_date}}</td>
+                            <td>{{$completedOrder->completed_date}}</td>
+                            <td>{{$completedOrder->reciever_fullName}}</td>
+                        </tr>
+                    @php $i += 1; @endphp
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            @php $i= 1; @endphp
+            <hr>
+            <div id="cancelledOrdersTable" class="mt-3">
+                <h4 class="text-center mt-2">Cancelled Orders</h4>
+                <table class="table-hover table-bordered w-100 text-center">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Buyer</th>
+                            <th scope="col">Order Date</th>
+                            <th scope="col">Cancel Date</th>
+                        </tr>
+                    <tbody>
+                    @foreach($cancelledOrders as $cancelledOrder)
+                        <tr>
+                            <td scope="row">{{$i}}</td>
+                            <td>{{$cancelledOrder->buyer_fullName}}</td>
+                            <td>{{$cancelledOrder->order_date}}</td>
+                            <td>{{$cancelledOrder->cancel_date ?? 'none'}}</td>
+                        </tr>
+                    @php $i += 1; @endphp
+                    @endforeach
+                    </tbody>
+                    </thead>
+                </table>
+            </div>
+            @php $i= 1; @endphp
+            <hr>
+            <div id="expiredOrdersTable" class="mt-3">
+                <h4 class="text-center mt-2">Expired Orders</h4>
+               <table class="table-hover table-bordered w-100 text-center">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Buyer</th>
+                            <th scope="col">Order Date</th>
+                            <th scope="col">Tag Date</th>
+                            <th scope="col">Completed By</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($expiredOrders as $expiredOrder)
+                        <tr>
+                            <td scope="row">{{$i}}</td>
+                            <td>{{$expiredOrder->buyer_fullName}}</td>
+                            <td>{{$expiredOrder->order_date}}</td>
+                            <td>{{$expiredOrder->completed_date}}</td>
+                            <td>{{$expiredOrder->reciever_fullName}}</td>
+                        </tr>
+                    @php $i += 1; @endphp
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+@elserole('Admin')
+            <div class="card">
+                <div class="card-header text-center">{{ __('Order Dashboard') }}</div>
+                <div class="card-body">
+                    <a href="/orders/scan" class="mb-2">
+                        <button class="btn btn-primary col-md-12 btn-lg mb-2">Scan QR</button>
+                    </a>
+                    <a href="/orders/e">
+                        <button class="btn btn-primary col-md-12 btn-lg">Tag Expired Orders</button>
+                    </a>
+                </div>
+            </div>
+@endrole
             <hr>
         </div>
     </div>
