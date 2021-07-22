0bc2d71b2a31e5ea02ba1a49f20275df94adbbd8
EarlCarolino <59614253+EarlCarolino@users.noreply.github.com>
2021-07-22 09:34:55 +0800

    Update last

--- resources/views/abtcont.blade.php
+++ resources/views/abtcont.blade.php
@@ -9,18 +9,20 @@
           <img src="/imgs/shoelala-logo2.svg" class="w-50 h-25 mb-5" alt="shoelala">
           <p class="card-text mb-5" style="line-height: 2;">Shoelala aims to provide an opportunity to every sneaker head to reserve and buy the sneaker they’ve been waiting for. Customers are our main focus, because they are what propels SHOELALA to expand our ideas, creativity, and collaborations.</p>
           <h5 class="display-2 mb-2">CONTACT SHOELALA</h5>
-          <a href="https://www.facebook.com/brynlsrn" class="card-link">
-              <img src="/imgs/fb-logo.png" alt="">
-          </a>
-          <a href="https://www.instagram.com/papi_bryn/" class="card-link">
-            <img src="/imgs/ig-logo.png" alt="">
-          </a>
-          <a href="https://twitter.com/papi_bryn" class="card-link">
-            <img src="/imgs/twt-logo.png" alt="">
-          </a>
-          <a href="mailto:lalashoe02@gmail.com" class="card-link">
-            <img src="/imgs/mail-logo.png" alt="">
-          </a>
+          <div class="contact-link mb-3">
+            <a href="https://www.facebook.com/brynlsrn" class="card-link">
+                <img src="/imgs/fb-logo.png" alt="">
+            </a>
+            <a href="https://www.instagram.com/papi_bryn/" class="card-link">
+              <img src="/imgs/ig-logo.png" alt="">
+            </a>
+            <a href="https://twitter.com/papi_bryn" class="card-link">
+              <img src="/imgs/twt-logo.png" alt="">
+            </a>
+            <a href="mailto:lalashoe02@gmail.com" class="card-link">
+              <img src="/imgs/mail-logo.png" alt="">
+            </a>
+          </div>
         </div>
     </div>
 </div>
